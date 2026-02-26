fc6e4e17cdb2da7dc8ce24017137455c1fe315ae
Nikki Murello <nmurello15@gmail.com>
2026-02-25 20:45:15 -0500

    Tweaked sphere hit code

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -12,14 +12,14 @@ Sphere::Sphere(
 
 float Sphere::CheckHit(const Ray& ray) const {
     Vec3f oc = center - ray.get_origin();
-    float a = Vec3f::dot(ray.get_direction(), ray.get_direction());
-    float b = -2.0f * Vec3f::dot(ray.get_direction(), oc);
-    float c = Vec3f::dot(oc, oc) - (radius * radius);
-    float descriminant = b * b - 4 * a * c;
+    float a = ray.get_direction().get_length_sq();
+    float h = Vec3f::dot(ray.get_direction(), oc);
+    float c = oc.get_length_sq() - radius * radius;
+    float descriminant = h * h - a * c;
 
     if (descriminant < 0) {
         return -1.0f;
     } else {
-        return (-b - std::sqrtf(descriminant)) / (2.0f * a);
+        return (h - std::sqrt(descriminant)) / a;
     }
 }
